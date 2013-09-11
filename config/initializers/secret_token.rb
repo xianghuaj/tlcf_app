cb3c092f586577a9b3815a4246694f07e54d38a4
xianghuaj <xianghua_j@yahoo.com>
2013-09-12 00:04:50 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TlcfApp::Application.config.secret_key_base = 'def0e525cb8d814d78d5c79af85dc03afe335bdb5fdcaccfc8a50758a4dd03c086e3614e656c2aa51e65bb71812676613f83faa73adb73e353f3e3faf6fc96dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TlcfApp::Application.config.session_store :cookie_store, key: '_tlcf_app_session'
